6ec9341f1d939ed8edadd9c390c03380f00d398c
Josh Piedimonte <joshpied@gmail.com>
2020-12-14 18:49:24 -0500

    Create for transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,5 +6,30 @@ class TransactionsController < ApplicationController
     # @transactions = current_user.transactions.joins("INNER JOIN transaction_categories ON transaction_categories.id = transactions.transaction_category_id").order(:id)
     # puts @transactions.to_json
   end
+
+  def show
+    @transaction = current_user.transactions.joins(:transaction_category).find(params[:id])
+  end
+
+  def new
+    @transaction = current_user.transactions.build
+  end
+  
+  def create
+    @transaction = current_user.transactions.build(transaction_params)
+    if @transaction.save
+      redirect_to transaction_path(@transaction), notice: "transaction Created!"
+    else
+      @errors = @transaction.errors.full_messages
+      render :new
+    end
+  end
+
+
+  private 
+
+  def transaction_params
+    params.require(:transaction).permit(:amount, :note, :transaction_category_id)
+  end
   
 end
