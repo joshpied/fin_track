3833479fc66fcee6196d1788e580e7363f4ff60d
Josh Piedimonte <joshpied@gmail.com>
2020-12-18 14:04:21 -0500

    Queries for reports page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,19 @@
 class ReportsController < ApplicationController
+  before_action :require_login 
+
   def index
-    @ = .all
+    current_month = Time.now.month
+    current_year = Time.now.year
+    @recent_report = current_user.reports.where("month = ? AND year = ?", current_month, current_year).first # most recent report object
+    @reports = current_user.reports.order("report_date desc") # reports array
+    # hash of transaction_category.name => transaction.amount spent this month
+    @transaction_categories = 
+      current_user.transactions
+      .where("report_id = ?", @recent_report.id)
+      .includes(:transaction_category)
+      .group("transaction_categories.name")
+      .sum(:amount)
+    
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,13 @@ Transaction.delete_all
 puts "Creating User"
 user = User.create(email: "test@test.com", password: "testers")
 
-report1 = Report.create(total_amount: 722.81, report_date: "2020-12-11 00:00:00", month: 12, year: 2020, user_id: user.id)
-report2 = Report.create(total_amount: 22003.55, report_date: "2020-12-11 00:00:00", month: 12, year: 2020, user_id: user.id)
+puts "Creating Reports"
+#november
+report1 = Report.create(total_amount: 22003.55, report_date: "2020-11-01 00:00:00", month: 11, year: 2020, user_id: user.id)
+#december
+report2 = Report.create(total_amount: 722.81, report_date: "2020-12-11 00:00:00", month: 12, year: 2020, user_id: user.id)
 
 puts "Creating Transaction Categories"
-# todo: add a color (https://tailwindcss.com/docs/customizing-colors) and icon to each category
 category_list = [
   "Travel",
   "Education",
@@ -47,10 +49,12 @@ category_list.each do |category|
 end
 
 puts "Creating Transactions"
-transaction1 = Transaction.create(note: "plane ticket", amount: 674.37, transaction_date: "2020-12-11 00:00:00", transaction_category_id: 1, user: user, report: report1)
-transaction2 = Transaction.create(note: "bought calculator", amount: 26.50, transaction_date: "2020-12-12 00:00:00", transaction_category_id: 2, user: user, report: report1)
-transaction3 = Transaction.create(note: "bus ticket", amount: 3.60, transaction_date: "2020-12-13 00:00:00", transaction_category_id: 3, user: user, report: report1)
-transaction4 = Transaction.create(note: "peace lily", amount: 18.34, transaction_date: "2020-12-15 00:00:00", transaction_category_id: 4, user: user, report: report1)
+# november
+transaction1 = Transaction.create(note: "bought a mazda", amount: 22000.00, transaction_date: "2020-11-01 00:00:00", transaction_category_id: 8, user: user, report: report1)
+transaction2 = Transaction.create(note: "crackerjacks", amount: 3.55, transaction_date: "2020-11-18 00:00:00", transaction_category_id: 15, user: user, report: report1)
+# december
+transaction3 = Transaction.create(note: "plane ticket", amount: 674.37, transaction_date: "2020-12-11 00:00:00", transaction_category_id: 1, user: user, report: report2)
+transaction4 = Transaction.create(note: "bought calculator", amount: 26.50, transaction_date: "2020-12-12 00:00:00", transaction_category_id: 2, user: user, report: report2)
+transaction5 = Transaction.create(note: "bus ticket", amount: 3.60, transaction_date: "2020-12-13 00:00:00", transaction_category_id: 3, user: user, report: report2)
+transaction6 = Transaction.create(note: "peace lily", amount: 18.34, transaction_date: "2020-12-15 00:00:00", transaction_category_id: 4, user: user, report: report2)
 
-transaction5 = Transaction.create(note: "bought a mazda", amount: 22000.00, transaction_date: "2020-11-01 00:00:00", transaction_category_id: 8, user: user, report: report2)
-transaction6 = Transaction.create(note: "crackerjacks", amount: 3.55, transaction_date: "2020-11-18 00:00:00", transaction_category_id: 15, user: user, report: report2)
