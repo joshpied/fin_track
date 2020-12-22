fbcf3dfa7ccb0bf7ad63c83c82e2465b960d0001
Josh Piedimonte <joshpied@gmail.com>
2020-12-22 10:53:48 -0500

    Styling reports view, partial for category badges

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,8 +4,18 @@ class ReportsController < ApplicationController
   def index
     current_month = Time.now.month
     current_year = Time.now.year
-    @recent_report = current_user.reports.where("month = ? AND year = ?", current_month, current_year).first # most recent report object
-    @reports = current_user.reports.order("report_date desc") # reports array
+    # most recent report object
+    @recent_report = 
+      current_user.reports
+      .where("month = ? AND year = ?", current_month, current_year)
+      .first 
+    # reports array (excluding recent report)
+    @reports = 
+      current_user.reports
+      .where
+      .not(id: @recent_report.id)
+      .order("report_date desc") 
+      # .pluck(:id, :report_date)
     # hash of transaction_category.name => transaction.amount spent this month
     @transaction_categories = 
       current_user.transactions
@@ -13,7 +23,19 @@ class ReportsController < ApplicationController
       .includes(:transaction_category)
       .group("transaction_categories.name")
       .sum(:amount)
-    
   end
+
+  def show
+    report_id = params[:id]
+    @report = current_user.reports.find(report_id)
+    @transactions = current_user.transactions.where("report_id = ?", report_id)
+    @transaction_categories = 
+      current_user.transactions
+      .where("report_id = ?", report_id)
+      .includes(:transaction_category)
+      .group("transaction_categories.name")
+      .sum(:amount)
+  end
+  
   
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,7 @@
 class Report < ApplicationRecord
   has_many :transactions
+
+  def to_s
+    report_date.strftime("%B %Y")
+  end
 end
